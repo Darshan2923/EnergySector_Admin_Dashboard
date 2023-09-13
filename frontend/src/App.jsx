16857313c7b280214d3ae4b7341008e708f2c48f
Darshan <darshanpatel2923@gmail.com>
2023-09-13 19:30:04 +0530

    Added a little navigation to pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
+import DashBoard from './components/DashBoard'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path='/' element={<Navigate to="/dashboard" replace />} />
+            <Route path='/dashboard' element={<DashBoard />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+    </div>
   )
 }
 
